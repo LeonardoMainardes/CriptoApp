3feecaa4d012cb9f4947fbe4b7ad09f31c7cf540
Leonardo Mainardes <leonardo.skora@cogecom.com.br>
2024-06-12 13:54:37 -0300

    First commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './header.module.css'
+
+export function Header(){
+    return(
+        <header className={styles.container}>
+            <h1>Header</h1>
+        </header>
+    )
+}
