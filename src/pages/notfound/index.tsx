a3fb4f3b9ee2f2cd614894d9068994f653473333
Leonardo Mainardes <leonardo.skora@cogecom.com.br>
2024-06-13 13:19:12 -0300

    jhv

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,7 +1,10 @@
+import styles from './notfound.module.css'
+
 export function NotFound(){
     return(
         <div>
-            <h1>404 Not Found</h1>
+            <h1 className={styles.not}>404 Not Found</h1>
         </div>
+        
     )
 }
